bf73e713e1936c09ab913d503615dce1442e76c7
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-28 22:20:12 +0100

    build(deps): update astro.config.mjs, package-lock.json and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  server: { port: 4321, host: true },
+  // server: { port: 4321, host: true },
   output: "hybrid",
-  vite: {
-    ssr: {
-      noExternal: ["modern-normalize"]
-    }
-  }
+  adapter: vercel()
+  // vite: {
+  //   ssr: {
+  //     noExternal: ["modern-normalize"]
+  //   }
+  // }
 });
