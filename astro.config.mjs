4dbc6e7a7a448245c0f346bba0dcd2d396a21432
Quentin GLX <quentin.glorieux@gmail.com>
2024-03-28 22:39:30 +0100

    update 1 file and delete 1 file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-// import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from "@astrojs/tailwind";
 
@@ -9,14 +8,14 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  // server: { port: 4321, host: true },
-  output: "hybrid"
-  // adapter: vercel()
-  // vite: {
-  //   ssr: {
-  //     noExternal: ["modern-normalize"]
-  //   }
-  // }
+  server: { port: 4321, host: true },
+  output: "hybrid",
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ["modern-normalize"]
+    }
+  }
   ,
-  adapter: vercel()
+  // adapter: vercel()
 });
